8e8ced71ba7d8c3efbec79d8d37f9f405382a6d5
Raphael Dussin <Raphael.Dussin@noaa.gov>
2019-12-11 17:07:25 -0500

    more tests and cleanup

--- static_downsampler/test/test_static_meta.py
+++ static_downsampler/test/test_static_meta.py
@@ -6,6 +6,7 @@ import os
 from static_downsampler.static import init_static_file
 from static_downsampler.static import subsample_supergrid
 from static_downsampler.static import correct_longitude_northfold
+from static_downsampler.static import sum_on_supergrid
 
 GRID_OM4_125 = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -52,3 +53,38 @@ def test_lonlat(datafiles, FIXTURE_DIR):
     static['geolat_c'] = subsample_supergrid(ds, 'y', 'Q')
     assert np.array_equal(static['geolon_c'].values, ds_check['geolon_c'].values)
     assert np.array_equal(static['geolat_c'].values, ds_check['geolat_c'].values)
+
+
+@pytest.mark.parametrize("FIXTURE_DIR", [GRID_OM4_125, GRID_OM4_025, GRID_OM4_05])
+def test_dxdy(datafiles, FIXTURE_DIR):
+    ds = xr.open_dataset(FIXTURE_DIR + 'ocean_hgrid.nc')
+    ds_check = xr.open_dataset(FIXTURE_DIR + 'ocean_static.nc')
+
+    static = init_static_file()
+    static['dxt'] = sum_on_supergrid(ds, 'dx', 'T', dimsum=('x'))
+    assert np.array_equal(static['dxt'].values, ds_check['dxt'].values)
+
+    static['dyt'] = sum_on_supergrid(ds, 'dy', 'T', dimsum=('y'))
+    assert np.array_equal(static['dyt'].values, ds_check['dyt'].values)
+
+    static['dxCu'] = sum_on_supergrid(ds, 'dx', 'U', dimsum=('x'))
+    static['dyCu'] = sum_on_supergrid(ds, 'dy', 'U', dimsum=('y'))
+    assert np.array_equal(static['dxCu'].values, ds_check['dxCu'].values)
+    # model cannot have zero dy but supergrid can, absolute tolerance is 2 meters
+    assert np.allclose(static['dyCu'].values, ds_check['dyCu'].values, atol=2.0)
+
+    static['dxCv'] = sum_on_supergrid(ds, 'dx', 'V', dimsum=('x'))
+    static['dyCv'] = sum_on_supergrid(ds, 'dy', 'V', dimsum=('y'))
+    assert np.array_equal(static['dxCv'].values, ds_check['dxCv'].values)
+    assert np.array_equal(static['dyCv'].values, ds_check['dyCv'].values)
+
+
+@pytest.mark.parametrize("FIXTURE_DIR", [GRID_OM4_125, GRID_OM4_025, GRID_OM4_05])
+def test_area(datafiles, FIXTURE_DIR):
+    ds = xr.open_dataset(FIXTURE_DIR + 'ocean_hgrid.nc')
+    ds_check = xr.open_dataset(FIXTURE_DIR + 'ocean_static.nc')
+
+    static = init_static_file()
+    static['areacello'] = sum_on_supergrid(ds, 'area', 'T', dimsum=('x', 'y'))
+    assert np.allclose(static['areacello'].values, ds_check['areacello'].values, atol=50000)
+
