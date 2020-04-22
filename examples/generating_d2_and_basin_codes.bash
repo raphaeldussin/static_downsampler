0b613aff61f4b87ffe679de166c8006ad41668c1
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-04-22 09:30:06 -0400

    update script

--- examples/generating_d2_and_basin_codes.bash
+++ examples/generating_d2_and_basin_codes.bash
@@ -7,3 +7,9 @@ make_basin_mask_d2.py -i ocean_static_no_mask_table.nc -o ocean_static.nc
 
 # 2. run downsampling
 downsample.py -s ocean_static.nc -o ocean_static_d2.nc
+
+# 3. create downsampled hgrid (needed for ocean_mosaic_d2)
+create_hgrid_d2.py -g ocean_hgrid.nc -o ocean_hgrid_d2.nc
+
+# 4. Create ocean_mosaic_d2
+make_solo_mosaic --num_tiles 1 --dir . --mosaic_name ocean_mosaic_d2 --tile_file ocean_hgrid_d2.nc
